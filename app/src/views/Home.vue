8c675c7538db5a28302e6982e4d0eaaa43c20ecf
Deteconan <dtconan01@gmail.com>
2021-07-09 12:21:32 -0400

    small fix

--- app/src/views/Home.vue
+++ app/src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
     <main-page>
         <div class="pb-5 pb-lg-10 pt-lg-0">
-            <div class="genres dark mb-5">
+            <div v-if="genres.length > 0" class="genres dark mb-5">
                 <genre-picker v-model="selectedGenre"></genre-picker>
             </div>
 
